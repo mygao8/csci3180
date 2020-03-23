f149b79f0157a39037b648f989f6109fa765f2a9
mygao8 <htz000114@gmail.com>
2020-03-23 13:42:45 +0800

    complete ta_ranking.c

--- CSCI3180/Assign1/ta_ranking.c
+++ CSCI3180/Assign1/ta_ranking.c
@@ -108,47 +108,55 @@ float calculate(char *course, char required[][16], char optional[][16], char ski
 //     }
 // }
 
-int addRes(struct result *result[], unsigned int sid, float score){
-    int i, j, ret = 0;
+void addRes(struct result *result, unsigned int sid, float score){
+    int i, j;
     // keep the pointer to last struct
-    struct result *tmp = result[2];
-
-    
-    for (int i = 0; i < 3; i++){
-        printf("%d %f\n", result[i]->sid, result[i]->score);
-    }
 
     printf("new %d %f\n", sid, score);
 
     // construct a new result structure for course[i] and candidate[j]
-    struct result *new = (struct result *)malloc(sizeof(struct result));
-    new->sid = sid;
-    new->score = score;
+    struct result new;
+    new.sid = sid;
+    new.score = score;
     
     // insert sort
     for (i = 2; i >= 0; i--){
         printf("i: %d\n", i);
-        if (new->score > result[i]->score || (new->score == result[i]->score && new->sid < result[i]->sid)){
-            for (j = 2-1; j >= i; j--){
-                result[j + 1] = result[j];
-            }
-            result[i] = new;
-            ret = 1;
+        if (new.score < result[i].score || (new.score == result[i].score && new.sid > result[i].sid)){
+            break;
         }
     }
 
-    // free eliminated candidate struct
-    if (ret == 1){
-        free(tmp);
+    // the i-th has higher priority than new
+    for (j = 2-1; j >= i+1; j--){
+        result[j + 1] = result[j];
     }
-    else{
-        free(new);
+    if (i + 1 < 3){
+        result[i+1] = new;
+    }
+}
+
+void output(char* course, FILE *fd, struct result *res){
+    char buf[11];
+
+    course[4] = ' ';
+    fwrite(course, sizeof(char), 5, fd);
+
+    for (int i = 0; i < 3; i++){
+        memset(buf, '0', sizeof(buf));
+        if (res[i].sid != 0){
+            sprintf(buf, "%d", res[i].sid);
+        }
+        // assign ' ' to the 11th char in buf 
+        buf[10] = ' ';
+        fwrite(buf, sizeof(buf), 1, fd);
     }
 
-    return ret;
+    fputc('\n', fd);
 }
 
 int main(){
+    int empty = 1;
     char buf[256];
 
     char course[5];
@@ -160,24 +168,21 @@ int main(){
     char skills[8][16];
     char prefer[3][5];
 
-    struct result *result[3];
+    struct result result[3];
 
-    //for (int i = 0; i < 3; i++){
-        memset(result[0], 0, sizeof(struct result));
-        memset(result[1], 0, sizeof(struct result));
-    
-    //     result[0]->score = 0;
-    // result[0]->sid = 0;
-    printf("begining\n");
+    memset(result, 0, sizeof(struct result) * 3);
 
-    for (int i = 0; i < 3; i++){
-        printf("%d %f\n",i, result[i]->score);
-    }
-    printf("OK at start\n");
     FILE *fdInstructor = fopen("instructors.txt", "rb");
     FILE *fdCandidate = fopen("candidates.txt", "rb");
 
     // if instructors.txt or candidates.txt doesn't exit
+    if (fdInstructor == NULL || fdCandidate == NULL){
+        printf("non-existing file!");
+        return (0);
+    }
+
+    FILE *fdOutput = fopen("output1.txt", "wb");
+
     // if instructors.txt is empty
     // if candidates.txt is empty
 
@@ -187,17 +192,22 @@ int main(){
             break;
         }
         else{
+            empty = 0;
             /*** read from instructors.txt ***/
 
             // required[i][14] == optional[i][14] == ' '
             // required[i][15] == optional[i][15] == '\0'
             // len(course) = 4
             storeInfo(buf, course, 4, required, optional, NULL);
+            printf("outer course:%s\n", course);
 
             /*** read from candidates.txt ***/
             float score;
+            fseek(fdCandidate, 0, SEEK_SET);
+            memset(result, 0, sizeof(struct result) * 3);
             while(1){
                 memset(buf, 0, sizeof(buf));
+
                 if (fgets(buf, sizeof(buf), fdCandidate) == NULL){
                     break;
                 }
@@ -207,18 +217,26 @@ int main(){
                     sid = atoi(strSid);
                     score = calculate(course, required, optional, skills, prefer);
                     // printf("%f\n", score);
-
+                    printf("course: %s, sid: %d, score: %f\n", course, sid, score);
                     // add score into result
                     addRes(result, sid, score);
+                    for (int i = 0; i < 3; i++){
+                        printf("%d %f\n", result[i].sid, result[i].score);
+                    }
                 }
             }
-
         }
+        printf("\nend a loop, courses: %s\n", course);
+        for (int i = 0; i < 3; i++){
+            printf("%d %f\n", result[i].sid, result[i].score);
+        }
+        printf("\n");
+
+        output(course, fdOutput, result);
     }
 
-
-
-    for (int i = 0; i < 3; i++){
-        printf("%d %f\n", result[i]->sid, result[i]->score);
-    }
+    // instructoris empty -> output is empty
+    fclose(fdOutput);
+    fclose(fdCandidate);
+    fclose(fdInstructor);
 }
